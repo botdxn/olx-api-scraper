173c48ed7440d759457f7b062ffe46d1f03a335c
Marcin Kucharski <dixone23@gmail.com>
2022-03-09 17:28:00 +0100

    Learning how to structure apps into packages

--- tests/test_scrape.py
+++ tests/test_scrape.py
@@ -1,5 +1,6 @@
 from olx-api-scraper.scrape import make_url, get_list_of_ads
 
+
 def test_scrape():
     assert isinstance(make_url("legnica", 500, 1000), str)
     assert isinstance(get_list_of_ads(make_url("legnica", 500, 1000), list):wq
